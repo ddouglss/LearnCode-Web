914efa81224bebd8ab0046289bd24e5d88e25b45
Douglas Souza <ddouglss1999@gmail.com>
2023-03-09 00:34:01 -0300

    Projeto em andamento

---
+++ LearnCodeAluno2/Models/Usuario.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace LearnCodeAluno2.Models
+{
+ //Estamos estudando design pattern POCO
+    [Table("Usuarios")]   //Pluralização - DataAnottations 
+    public class Usuario
+    {
+        public int Id { get; set; } // Propriedade automatica 
+
+        [Required]
+        [MaxLength(100)]
+        public string Nome { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Login { get; set; }
+
+        [Required]
+        [MaxLength(60)]
+        public string Senha { get; set; }
+
+    }
+}
---
+++ LearnCodeAluno2/Startup1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+
+[assembly: OwinStartup(typeof(LearnCodeAluno2.Startup1))]
+
+namespace LearnCodeAluno2
+{
+    public class Startup1
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions // Metodo pra realizar a Autentificação
+            {
+                //Ctrl+espaço ele mostra as propriedades que já estão definidas aqui na aplicação
+                AuthenticationType = "ApplicationCookie",
+                //Rota (Endereço,URL)
+                LoginPath= new PathString("/Atenticacao/Login")
+            }); 
+        }
+    }
+}
